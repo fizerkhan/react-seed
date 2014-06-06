d877469aeb2883dddf3051ce24707486f36a163d
Fizer Khan <fizerkhan@gmail.com>
2014-06-06 11:47:18 +0530

    Removed live reload. Added image folder.

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,9 +4,15 @@ var gulp       = require('gulp'),
     imagemin   = require('gulp-imagemin'),
     sass       = require('gulp-sass'),
     watch      = require('gulp-watch'),
-    connect    = require('gulp-connect');
+    connect    = require('gulp-connect'),
+    clean = require('gulp-clean');
 
 var ROOT = __dirname + '/build'
+
+gulp.task('clean', function () {
+ return gulp.src('build/', {read: false})
+        .pipe(clean());
+});
 
 gulp.task('styles', function () {
   gulp.src('assets/scss/main.scss')
@@ -24,7 +30,7 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('images', function () {
-  gulp.src(['assets/img/**/*.png', 'assets/img/**/*.gif'])
+  gulp.src(['assets/img/**/*.png', 'assets/img/**/*.jpg', 'assets/img/**/*.gif'])
       .pipe(imagemin())
       .pipe(gulp.dest('build/img/'));
 });
@@ -41,19 +47,22 @@ gulp.task('watch', function() {
   gulp.watch('*.html', [ 'copy' ]);
 });
 
-gulp.task('livereload', function() {
-  gulp.src(['build/css/*.css', 'build/js/*.js', 'build/img/*', 'build/index.html'])
-    .pipe(watch())
-    .pipe(connect.reload());
-});
+// gulp.task('livereload', function() {
+//   gulp.src(['build/css/*.css', 'build/js/*.js', 'build/img/*', 'build/index.html'])
+//     .pipe(watch())
+//     .pipe(connect.reload());
+// });
 
 gulp.task('webserver', function() {
   connect.server({
-    livereload: true,
+    livereload: false,
     port: 8000,
     root: ['build']
   });
 });
 
-gulp.task('build', [ 'styles', 'scripts', 'images', 'copy' ]);
-gulp.task('default', ['build', 'webserver', 'livereload', 'watch']);
+gulp.task('build', [ 'clean', 'styles', 'scripts', 'images', 'copy' ]);
+gulp.task('default', ['build', 'webserver', 'watch']);
+
+// Live reload has an issue of loading react.js routes
+// gulp.task('default', ['build', 'webserver', 'livereload', 'watch']);
